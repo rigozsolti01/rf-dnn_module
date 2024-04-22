86843173f2eaafe3917ec4bf5beacc9e1d1871f9
Rigó Zsolt <rigozsolt690@gmail.com>
2024-04-22 21:56:00 +0200

    Working View and Model

---
+++ Mantis_Support/Controllers/SendTicketController.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Mantis.Support.Module.Dnn.Mantis_Support.Models;
+
+public class SendTicketController : DnnController
+{
+    [HttpPost]
+    public async Task<ActionResult> SendMessage(TicketData formData)
+    {
+        var jsonBody = new
+        {
+            summary = string.Format("[Domania_SUPPORT] - {0}", formData.Subject),
+            description = string.Format("Sender: {0}\nEmail: {1}\nMessage: {2}", formData.Name, formData.Email, formData.Message),
+            project = new { id = 49, name = "Forró Torták" },
+            category = new { id = 5, name = "Design" }, 
+            handler = new { id = 146, name = "therigozsolti" },
+            view_state = new { id = 10, name = "public" },
+            priority = new { id = 30, name = "normal" },
+            severity = new { id = 10, name = "kérés" },
+            reproducibility = new { name = "nem próbáltam" }
+        };
+
+        var json = JsonConvert.SerializeObject(jsonBody);
+        var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Authorization", "aS-X0G5twCHlaJjdsnq0ruiugKLl5kkY");
+
+            var response = await client.PostAsync("https://fr.uni-corvinus.hu/mantis/api/rest/issues", data);
+            var result = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Handle success
+                return Json(new { success = true, message = "Message sent!" });
+            }
+            else
+            {
+                // Handle failure
+                return Json(new { success = false, message = "Failed to send message." });
+            }
+        }
+    }
+}
---
+++ Mantis_Support/Models/TicketData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mantis.Support.Module.Dnn.Mantis_Support.Models
+{
+    public class TicketData
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
